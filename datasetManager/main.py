0e9c3a25ecf4eeb5a9457f3557f27255d78b3283
galassmyname <falloumyname@gmail.com>
2026-08-01 17:37:33 +0000

    Création du menu interactif et mise a jour du fichier

--- datasetManager/main.py
+++ datasetManager/main.py
@@ -1,38 +1,26 @@
-# Partie 1 : Types de base, variables, Entrées et sorties
-# Saisie des métadonnées
-nom = input("Nom du dataset : ")
-domaine = input("Domaine : ")
-
-nb_lignes = int(input("Nombre de lignes : "))
-nb_colonnes = int(input("Nombre de colonnes : "))
-taille = float(input("Taille (en Mo) : "))
-
-# Vérification du format
+# Partie 2 : Structures de contrôle
 while True:
-    format_fichier = input("Format (csv ou json) : ").lower()
-    if format_fichier == "csv" or format_fichier == "json":
-        break
-    else:
-        print("Erreur : le format doit être csv ou json.")
+    print("\n========================")
+    print("1. Ajouter un dataset")
+    print("2. Afficher les datasets")
+    print("3. Rechercher")
+    print("4. Quitter")
+    print("==================")
 
-# Vérification de public
-while True:
-    public_input = input("Public (true ou false) : ").lower()
-    if public_input == "true":
-        public = True
-        break
-    elif public_input == "false":
-        public = False
-        break
-    else:
-        print("Erreur : veuillez saisir true ou false.")
+    choix = input("Votre choix : ")
 
-# Affichage du résumé
-print("\n RÉSUMÉ DU DATASET ")
-print(f"Nom                 : {nom}")
-print(f"Domaine             : {domaine}")
-print(f"Nombre de lignes    : {nb_lignes}")
-print(f"Nombre de colonnes  : {nb_colonnes}")
-print(f"Taille              : {taille} Mo")
-print(f"Format              : {format_fichier}")
-print(f"Public              : {public}")
+    if choix == "1":
+        print("Ajout d'un dataset.")
+
+    elif choix == "2":
+        print("Affichage des datasets.")
+
+    elif choix == "3":
+        print("Recherche d'un dataset.")
+
+    elif choix == "4":
+        print("Vous avez quitté le programme !")
+        break
+
+    else:
+        print("Choix invalide. Veuillez choisir entre 1 et 4.")
